6205aa0cd1b027a3c0704d1bbdc74ab93b9a6917
vladyn <vlado@vlado1.com>
2017-12-16 22:17:41 +0100

    few more console.log's

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ import 'babel-polyfill';
 import './styles/armed.scss';
 import * as armed from './scripts/armed';
 
-
-// just a test
 console.log("armed is: " + armed);
--- scripts/armed.js
+++ scripts/armed.js
@@ -1,7 +1,6 @@
 const armed = (function () {
     let track1 = document.getElementById("track-1");
-    console.log("track 1 is " + track1.toLocaleString());
-
+    console.log("track 1 is " + track1);
 }());
 
 export default armed;
