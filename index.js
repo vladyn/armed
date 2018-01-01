597a6f688540d8ad386a92261e0c3ace0b1509b4
vladyn <vlado@vlado1.com>
2018-01-01 02:03:22 +0200

    Adding small test with alert

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import 'babel-polyfill';
-import './styles/armed.scss';
-import 'sweetalert2';
+import './styles/armed.css';
+import swal from 'sweetalert2'
 import 'amplitudejs';
 import * as armed from './scripts/armed';
 
 console.log('imported: ', armed);
+
+const alert = document.getElementById('alert-1');
+alert.addEventListener('click', swal(
+    'Good job!',
+    'You clicked the button!',
+    'success'
+));
